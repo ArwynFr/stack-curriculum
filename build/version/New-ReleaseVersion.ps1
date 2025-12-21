25124b77d90c9b32a831886d355e31f9ca939af3
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2025-12-21 17:21:32 +0100

    ci: migrate from kubernetes to helm charts

---
+++ build/version/New-ReleaseVersion.ps1
@@ -0,0 +1,10 @@
+[version]$CurrentDate = [version]::new((Get-Date -Format yy), (Get-Date).DayOfYear, 0)
+[version]$CurrentVersion = (gh release list --limit 1 --json tagName --jq '.[0].tagName')
+
+$TargetVersion = if ($CurrentVersion -ge $CurrentDate) {
+  [version]::new($CurrentVersion.Major, $CurrentVersion.Minor, $CurrentVersion.Build + 1)
+} else { 
+  $CurrentDate
+}
+
+return $TargetVersion.ToString()
